038742d55c6140639c0039ae06ead683129e16ba
Welson Oktario <welsonoktario@yahoo.com>
2023-04-06 01:00:52 +0700

    Update config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,8 @@
 import Layout from '@components/layout/layout';
-
+import '@styles/global.css';
 import type { AppProps } from 'next/app';
 
-import '@styles/global.css';
-
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
