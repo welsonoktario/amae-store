2287c0f0f3e713d9d4fd3e4d559e64b229de67f4
welsonoktario <welsonoktario@yahoo.com>
2023-04-15 13:46:32 +0700

    Update lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,6 +5,12 @@ const buildEslintCommand = (filenames) =>
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`;
 
+const buildPrettierCommand = (filenames) =>
+  `prettier --write ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')}`;
+
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildPrettierCommand],
+  '*.{json,css,md}': [buildPrettierCommand],
 };
